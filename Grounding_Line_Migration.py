b1dfb2067f229f67aaa00a7342099f9a6ea8248a
Duncan Byrne <byrne@mines.edu>
2023-01-29 13:21:39 -0700

    Added across track slope capture

--- Grounding_Line_Migration.py
+++ Grounding_Line_Migration.py
@@ -5,6 +5,8 @@ import geopandas as gpd
 import numpy as np
 import math
 from cartopy import crs as ccrs
+from polar_convert.constants import SOUTH
+from polar_convert import polar_lonlat_to_xy
 
 #path = r'C:\Users\Nacnu\Documents\IceSat2\backgrounddata\ATA_adm0.shp'
 #basemap = unpack.Basemap(path)
@@ -16,7 +18,8 @@ dataset = unpack.Dataset(r"C:\Users\Nacnu\Documents\IceSat2")
 
 granule = dataset.openfilename("ATL06_20181114050603_07110110_005_01.h5")
 lat, lon, time, dh_fit_dx, dh_fit_dx_sigma, metadata, granuledata = granule.laser1l.getTrackData()
-azumith = granule.laser1l.returnAzumith()
+azumith = granule.laser1l.returnAzimuth()
+dh_fit_dy = granule.laser1l.returnAcrossTrackSlope()
 
 def latlonindex_to_grid(lat, lon, time, dh_fit_dx, dh_fit_dx_sigma):
     crs = ccrs.SouthPolarStereo()
@@ -65,9 +68,40 @@ def rearrange_angle_data(flowdatabase, xyindices):
     except:
         raise Exception("Angle data for flowdatabase does not exist")
 
+def angNorth_to_angXY(coord, angle):
+    lon, lat = coord
+    wgsvec = (lon + math.cos(angle), lat + math.sin(angle))
+    true_scale_lat = 71
+    re = 6378.137
+    e = 0.01671
+    hemisphere = SOUTH
+    origxy = polar_lonlat_to_xy(lon, lat, true_scale_lat, re, e, hemisphere)
+    newxy = polar_lonlat_to_xy(wgsvec[0], wgsvec[1], true_scale_lat, re, e, hemisphere)
+    vecxy = [newxy[i] - origxy[i] for i in range(2)]
+    angle = math.atan(vecxy[1]/vecxy[0])
+    return angle
+
+def angleTransform(lon, lat, azumith):
+    angles= np.array([angNorth_to_angXY((lon[i], lat[i]), azumith[i]) for i in range(len(lon))])
+    return angles
+
+# known to work perfectly.
 def calc_along_flow_slope(dh_fit_dx, dh_fit_dy, dh_fit_dx_sigma, slope_azumith, flow_ang, flow_ang_err):
-    
-    pass
+    slopevec = np.array([math.cos(slope_azumith), math.sin(slope_azumith), dh_fit_dx])
+    acrossslopevec = np.array([math.sin(slope_azumith), -1*math.cos(slope_azumith), dh_fit_dy])
+    flowvec = np.array([math.cos(flow_ang), math.sin(flow_ang), 0])
+    slopeflowvec = (flowvec.dot(slopevec)/slopevec.dot(slopevec))*slopevec+(flowvec.dot(acrossslopevec)/acrossslopevec.dot(acrossslopevec))*acrossslopevec
+    slopeflowgrade = slopeflowvec[2] / math.sqrt(slopeflowvec[0]**2 + slopeflowvec[1]**2)
+    if dh_fit_dy >= 1e30:
+        return None
+    else:
+        return slopeflowgrade
+
+def get_flow_slopes(dh_fit_dx, dh_fit_dy, dh_fit_dx_sigma, slope_azumith, flow_ang, flow_ang_err):
+    along_flow_slope = np.array([calc_along_flow_slope(dh_fit_dx[i], dh_fit_dy[i], dh_fit_dx_sigma[i], slope_azumith[i], flow_ang[i], flow_ang_err[i]) for i in range(len(flow_ang))])
+    return along_flow_slope
 
 xyindices = latlonindex_to_grid(lat, lon, time, dh_fit_dx, dh_fit_dx_sigma)
-ANG, ANGERR = rearrange_angle_data(flowdatabase, xyindices)
+flow_angle, flow_angle_error = rearrange_angle_data(flowdatabase, xyindices)
+azumith_in_xy = angleTransform(lon, lat, azumith)
+flowslopes = get_flow_slopes(dh_fit_dx, dh_fit_dy, dh_fit_dx_sigma, azumith_in_xy, flow_angle, flow_angle_error)
